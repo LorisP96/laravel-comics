efc20be8a8909d83d59a7b8fd204fc5a9eab2017
LorisP96 <lorispalazzo96@gmail.com>
2022-08-24 18:16:28 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,40 @@
+<header>
+    <div class="flex-between">
+        <div class="logo">
+            <img src="" alt="brand-logo">
+        </div>
+        <ul class="flex-evenly">
+            <li>
+                <a href="">CHARACTERS</a>
+            </li>
+            <li>
+                <a href="">COMICS</a>
+            </li>
+            <li>
+                <a href="">MOVIES</a>
+            </li>
+            <li>
+                <a href="">TV</a>
+            </li>
+            <li>
+                <a href="">GAMES</a>
+            </li>
+            <li>
+                <a href="">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="">VIDEOS</a>
+            </li>
+            <li>
+                <a href="">FANS</a>
+            </li>
+            <li>
+                <a href="">NEWS</a>
+            </li>
+            <li>
+                <a href="">SHOP</a>
+            </li>
+        </ul>
+    </div>
+  </header>
+  
---
+++ resources/views/partials/jumbo.blade.php
@@ -0,0 +1,5 @@
+<section class="jumbo">
+    <div class="series-cont">
+        <h4>CURRENT SERIES</h4>
+    </div>  
+</section>
